4aba2ef53cb491105499be812bcf9358010fb624
visualkim <visual@adup.kr>
2016-06-04 11:29:46 +0900

    aa

---
+++ app/src/main/java/com/example/cypri/myapp2/KaupService.java
@@ -0,0 +1,9 @@
+package com.example.cypri.myapp2;
+
+/**
+ * Created by cypri on 2016-06-04.
+ */
+public class KaupService {
+    String txtName, txtResult;
+    double txtWeight, txtHeight;
+}
--- app/src/main/java/com/example/cypri/myapp2/MainActivity.java
+++ app/src/main/java/com/example/cypri/myapp2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.cypri.myapp2;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -11,25 +12,51 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private EditText editName;
     private EditText editWeight;
     private EditText editHeight;
     private Button btnCheck;
+    TextView tvResult;
+
+    String txtName, txtResult;
+    double txtWeight, txtHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         editName = (EditText) findViewById(R.id.editName);
         editWeight = (EditText) findViewById(R.id.editWeight);
         editHeight = (EditText) findViewById(R.id.editHeight);
         btnCheck = (Button) findViewById(R.id.btnCheck);
+
+        txtName = editName.getText().toString();
+        txtWeight = Double.parseDouble( editWeight.getText().toString() );
+        txtHeight = Double.parseDouble( editHeight.getText().toString() );
+
+        int idx = (int)(txtHeight / (txtHeight * txtHeight )) * 10000;
+
+        if( idx >= 30 ) {
+            txtResult = "비만";
+        } else if( idx >= 24 ) {
+            txtResult = "과체중";
+        } else if( idx >= 20 ) {
+            txtResult = "정상";
+        } else if( idx >= 15 ) {
+            txtResult = "저체중";
+        } else if( idx >= 13 ) {
+            txtResult = "마름";
+        } else if( idx >= 10 ) {
+            txtResult = "영양실조";
+        } else {
+            txtResult = "소모증";
+        }
+        tvResult.setText( txtName + "님의 계산결과: " + txtResult );
 
         btnCheck.setOnTouchListener(new View.OnTouchListener() {
             @Override
