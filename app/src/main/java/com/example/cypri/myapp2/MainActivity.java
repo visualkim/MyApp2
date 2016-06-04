7b2e325736f8d14e622c38ab49bca8b4697a85a3
visualkim <visual@adup.kr>
2016-06-04 10:24:02 +0900

    aa

--- app/src/main/java/com/example/cypri/myapp2/MainActivity.java
+++ app/src/main/java/com/example/cypri/myapp2/MainActivity.java
@@ -5,11 +5,19 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editName;
+    private EditText editWeight;
+    private EditText editHeight;
+    private Button btnCheck;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +26,16 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        editName = (EditText) findViewById(R.id.editName);
+        editWeight = (EditText) findViewById(R.id.editWeight);
+        editHeight = (EditText) findViewById(R.id.editHeight);
+        btnCheck = (Button) findViewById(R.id.btnCheck);
+
+        btnCheck.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
     }
 }
